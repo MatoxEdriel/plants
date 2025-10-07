9e8e78513fc6f83cf85ab4456cebc8de9a98c77d
MatoxEdriel <ga99_buggi@hotmail.com>
2025-10-06 20:52:00 -0500

    configuracion para microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
-  console.log(`Application runnning to the PORT ${envs.port}`);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: envs.port,
+      },
+    },
+  );
+  await app.listen();
+  console.log(`Microservice on the port ${envs.port}`);
 }
 bootstrap();
